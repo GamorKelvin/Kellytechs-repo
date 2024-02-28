50414c6897f8390134ca76317a831cc131bd395b
GamorKelvin <158982626+GamorKelvin@users.noreply.github.com>
2024-02-28 23:03:52 +0000

    DataScienceEcosystem.ipynb
    A little assessment from me

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools And Ecosystem
+
+# In this notebook ,Data Science Tolls and Ecosystem are summarized
+
+# Some of the polular languages data scientist use are:
+#     1. Python
+#     2. R Language
+#     3.Julia
+#     
+
+# some of the commonly libraries used by data scientist are:
+
+# 1.Numpy
+# 2.seaborn 
+# 3.sklearn
+# 
+
+# |.......................|
+# | Data Science Tools    |
+# 
+
+# ### Below are few examples of evaluating arithemetic expression in python:
+
+# In[13]:
+
+
+28*3
+
+
+# In[14]:
+
+
+2+3
+
+
+# This is a simple arithemetic expression to add the integers
+
+# In[15]:
+
+
+(3*4)+5
+
+
+# This will convert 200 minutes to hours by deviding it by 60
+
+# In[16]:
+
+
+200/60
+
+
+# List popular Languages for Data Science
+
+# * Python
+# * R Language
+# * Julia
+
+# ## This author's name is Gamor Kelvin
+
+# In[ ]:
+
+
+
+
